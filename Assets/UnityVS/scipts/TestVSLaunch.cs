7b90c4f99cf6a3119c348a9d2197d0cecdf38d96
Martin Smith <martinsmith160@yahoo.co.uk>
2016-08-16 20:25:23 +0100

    first commit

---
+++ Assets/UnityVS/scipts/TestVSLaunch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestVSLaunch : MonoBehaviour
+{
+    public bool test_bool;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
